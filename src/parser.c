fed4d56283fdee4b39f43869402b7990e1e9c2eb
Vasileios Vrouliotis <vvrouliotis@gmail.com>
2019-06-05 06:55:33 +0300

    Fixed return:sub expressions.

--- src/parser.c
+++ src/parser.c
@@ -1123,14 +1123,12 @@ parse_state* checkTheStack(parse_state* current, char* theStackTop, int top, com
 							return current;
 						}
 						else {
-
 							if ((tempOp2 != NULL) && (tempOp2[0] != '\0')) {
 								printf("additional saved operator : %s\n", tempOp2);
 								++tempTop;
 								push(tempStack[tempTop], &tempTop, tempOp2);
 								continue;
 							}
-
 							++tempTop;
 							push(tempStack[tempTop], &tempTop, current->value);
 							strcpy(temp, current->value);
@@ -1230,7 +1228,9 @@ parse_state* checkTheStack(parse_state* current, char* theStackTop, int top, com
 							else if (strcmp(tempStack[tempTop], "-") == 0) {
 								expr* subExpr = expr_create(EXPR_SUB, leftExpr, rightExpr, 0, '\0', NULL);
 								operatorUsed = 1;
-								printf("the operator used add\n");
+								printf("the operator used sub\n");
+								//copying right id expr to become new left id expr
+								strcpy(temp, current->value);
 								push_expressionList(expressionListNode, subExpr);
 								current = current->next;
 								if (current == NULL) {
@@ -1243,7 +1243,9 @@ parse_state* checkTheStack(parse_state* current, char* theStackTop, int top, com
 							else if (strcmp(tempStack[tempTop], "*") == 0) {
 								expr* mulExpr = expr_create(EXPR_MUL, leftExpr, rightExpr, 0, '\0', NULL);
 								operatorUsed = 1;
-								printf("the operator used add\n");
+								printf("the operator used mul\n");
+								//copying right id expr to become new left id expr
+								strcpy(temp, current->value);
 								push_expressionList(expressionListNode, mulExpr);
 								current = current->next;
 								if (current == NULL) {
@@ -1256,7 +1258,9 @@ parse_state* checkTheStack(parse_state* current, char* theStackTop, int top, com
 							else if (strcmp(tempStack[tempTop], "/") == 0) {
 								expr* divExpr = expr_create(EXPR_DIV, leftExpr, rightExpr, 0, '\0', NULL);
 								operatorUsed = 1;
-								printf("the operator used add\n");
+								printf("the operator used div\n");
+								//copying right id expr to become new left id expr
+								strcpy(temp, current->value);
 								push_expressionList(expressionListNode, divExpr);
 								current = current->next;
 								if (current == NULL) {
@@ -1271,6 +1275,8 @@ parse_state* checkTheStack(parse_state* current, char* theStackTop, int top, com
 									expr* addExpr = expr_create(EXPR_ADD, leftExpr, rightExpr, 0, '\0', NULL);
 									operatorUsed = 1;
 									printf("the operator used add\n");
+									//copying right id expr to become new left id expr
+									strcpy(temp, current->value);
 									push_expressionList(expressionListNode, addExpr);
 									current = current->next;
 									if (current == NULL) {
@@ -1284,7 +1290,9 @@ parse_state* checkTheStack(parse_state* current, char* theStackTop, int top, com
 								else if (strcmp(tempOp2, "-") == 0) {
 									expr* subExpr = expr_create(EXPR_SUB, leftExpr, rightExpr, 0, '\0', NULL);
 									operatorUsed = 1;
-									printf("the operator used add\n");
+									printf("the operator used sub\n");
+									//copying right id expr to become new left id expr
+									strcpy(temp, current->value);
 									push_expressionList(expressionListNode, subExpr);
 									current = current->next;
 									if (current == NULL) {
@@ -1297,7 +1305,9 @@ parse_state* checkTheStack(parse_state* current, char* theStackTop, int top, com
 								else if (strcmp(tempOp2, "*") == 0) {
 									expr* mulExpr = expr_create(EXPR_MUL, leftExpr, rightExpr, 0, '\0', NULL);
 									operatorUsed = 1;
-									printf("the operator used add\n");
+									printf("the operator used mul\n");
+									//copying right id expr to become new left id expr
+									strcpy(temp, current->value);
 									push_expressionList(expressionListNode, mulExpr);
 									current = current->next;
 									if (current == NULL) {
@@ -1310,7 +1320,9 @@ parse_state* checkTheStack(parse_state* current, char* theStackTop, int top, com
 								else if (strcmp(tempOp2, "/") == 0) {
 									expr* divExpr = expr_create(EXPR_DIV, leftExpr, rightExpr, 0, '\0', NULL);
 									operatorUsed = 1;
-									printf("the operator used add\n");
+									printf("the operator used div\n");
+									//copying right id expr to become new left id expr
+									strcpy(temp, current->value);
 									push_expressionList(expressionListNode, divExpr);
 									current = current->next;
 									if (current == NULL) {
@@ -1325,9 +1337,7 @@ parse_state* checkTheStack(parse_state* current, char* theStackTop, int top, com
 								}
 							}
 						}
-
 					}
-
 					if (strcmp(current->type, "end of command") == 0) {
 						current = current->next;
 						if (current != NULL) {
@@ -1369,12 +1379,9 @@ parse_state* checkTheStack(parse_state* current, char* theStackTop, int top, com
 								else {
 
 								}
-
-
 							}
 						}
 					}
-
 				}
 				else if (strcmp(tempStack[tempTop], "-") == 0) {
 					//tempStack[0][tempTop] = '\0';
@@ -1386,12 +1393,11 @@ parse_state* checkTheStack(parse_state* current, char* theStackTop, int top, com
 						pop(&tempTop);
 						pop(&tempTop);
 						printf("now in the stack in sub already id: %s\n", tempStack[tempTop]);
-
 						expr* leftExpr = expr_create_string(tempStack[tempTop]);
 						expr* rightExpr = expr_create_string(current->value);
 						expr* subExpr = expr_create(EXPR_SUB, leftExpr, rightExpr, 0, '\0', NULL);
 						push_expressionList(expressionListNode, subExpr);
-						printf("pushin addExpr in expressionListNode!!!!!!!!!!!\n");
+						printf("pushin subExpr in expressionListNode!!!!!!!!!!!\n");
 						operatorInsideStack = 0;
 
 						//pushin to stack the previous id
@@ -1407,14 +1413,12 @@ parse_state* checkTheStack(parse_state* current, char* theStackTop, int top, com
 							return current;
 						}
 						else {
-
-							if (tempOp2 != NULL) {
+							if ((tempOp2 != NULL) && (tempOp2[0] != '\0')) {
 								printf("additional saved operator : %s\n", tempOp2);
 								++tempTop;
 								push(tempStack[tempTop], &tempTop, tempOp2);
 								continue;
 							}
-
 							++tempTop;
 							push(tempStack[tempTop], &tempTop, current->value);
 							strcpy(temp, current->value);
@@ -1434,8 +1438,13 @@ parse_state* checkTheStack(parse_state* current, char* theStackTop, int top, com
 							++tempTop;
 							push(tempStack[tempTop], &tempTop, current->value);
 							strcpy(tempOp2, current->value);
+							if (operatorUsed == 1) {
+								strcpy(tempOp, current->value);
+							}
 							current = current->next;
 							printf("now the current 1.2 : %s\n", current->value);
+							printf("now the temp : %s\n", temp);
+							printf("now the tempOp : %s\n", tempOp);
 
 							if (strcmp(tempOp, "+") == 0) {
 								printf("going to add, add expression to the list\n");
@@ -1468,23 +1477,35 @@ parse_state* checkTheStack(parse_state* current, char* theStackTop, int top, com
 							else {
 
 							}
-;
 							strcpy(temp, current->value);
 							strcpy(tempOp, tempOp2);
 							current = current->next;
-							printf("now the current 1.3 : %s\n", current->value);
+
+							if (current == NULL) {
+								printf("current is NULL operator again sub\n");
+								stmt* ret_stmt = stmt_create(STMT_RETURN, NULL, NULL, NULL, NULL, NULL, NULL, expressionListNode, NULL);
+								push_commandList(commandNode, NULL, ret_stmt, NULL);
+								return current;
+							}
+
+							printf("now the current 1.3 sub : %s\n", current->value);
 							continue;
 						}
 						else if (strcmp(current->type, "identifier") == 0) {
 							printf("second parameter sub %s\n", current->value);
-
 							printf("the temp : %s\n", temp);
 							printf("now in the stack : %s\n", tempStack[tempTop]);
+							printf("operator : %s\n", tempOp2);
+
 							expr* leftExpr = expr_create_string(temp);
 							expr* rightExpr = expr_create_string(current->value);
 
 							if (strcmp(tempStack[tempTop], "+") == 0) {
 								expr* addExpr = expr_create(EXPR_ADD, leftExpr, rightExpr, 0, '\0', NULL);
+								operatorUsed = 1;
+								printf("the operator used add\n");
+								//copying right id expr to become new left id expr
+								strcpy(temp, current->value);
 								push_expressionList(expressionListNode, addExpr);
 								current = current->next;
 								if (current == NULL) {
@@ -1493,10 +1514,13 @@ parse_state* checkTheStack(parse_state* current, char* theStackTop, int top, com
 									push_commandList(commandNode, NULL, ret_stmt, NULL);
 									return current;
 								}
-								
 							}
 							else if (strcmp(tempStack[tempTop], "-") == 0) {
 								expr* subExpr = expr_create(EXPR_SUB, leftExpr, rightExpr, 0, '\0', NULL);
+								operatorUsed = 1;
+								printf("the operator used sub\n");
+								//copying right id expr to become new left id expr
+								strcpy(temp, current->value);
 								push_expressionList(expressionListNode, subExpr);
 								current = current->next;
 								if (current == NULL) {
@@ -1508,24 +1532,106 @@ parse_state* checkTheStack(parse_state* current, char* theStackTop, int top, com
 							}
 							else if (strcmp(tempStack[tempTop], "*") == 0) {
 								expr* mulExpr = expr_create(EXPR_MUL, leftExpr, rightExpr, 0, '\0', NULL);
+								operatorUsed = 1;
+								printf("the operator used mul\n");
+								//copying right id expr to become new left id expr
+								strcpy(temp, current->value);
 								push_expressionList(expressionListNode, mulExpr);
+								current = current->next;
+								if (current == NULL) {
+									printf("current is NULL\n");
+									stmt* ret_stmt = stmt_create(STMT_RETURN, NULL, NULL, NULL, NULL, NULL, NULL, expressionListNode, NULL);
+									push_commandList(commandNode, NULL, ret_stmt, NULL);
+									return current;
+								}
 							}
 							else if (strcmp(tempStack[tempTop], "/") == 0) {
 								expr* divExpr = expr_create(EXPR_DIV, leftExpr, rightExpr, 0, '\0', NULL);
+								operatorUsed = 1;
+								printf("the operator used div\n");
+								//copying right id expr to become new left id expr
+								strcpy(temp, current->value);
 								push_expressionList(expressionListNode, divExpr);
+								current = current->next;
+								if (current == NULL) {
+									printf("current is NULL\n");
+									stmt* ret_stmt = stmt_create(STMT_RETURN, NULL, NULL, NULL, NULL, NULL, NULL, expressionListNode, NULL);
+									push_commandList(commandNode, NULL, ret_stmt, NULL);
+									return current;
+								}
 							}
 							else {
+								if (strcmp(tempOp2, "+") == 0) {
+									expr* addExpr = expr_create(EXPR_ADD, leftExpr, rightExpr, 0, '\0', NULL);
+									operatorUsed = 1;
+									printf("the operator used add\n");
+									//copying right id expr to become new left id expr
+									strcpy(temp, current->value);
+									push_expressionList(expressionListNode, addExpr);
+									current = current->next;
+									if (current == NULL) {
+										printf("current is NULL\n");
+										stmt* ret_stmt = stmt_create(STMT_RETURN, NULL, NULL, NULL, NULL, NULL, NULL, expressionListNode, NULL);
+										push_commandList(commandNode, NULL, ret_stmt, NULL);
+										return current;
+									}
+									
+								}
+								else if (strcmp(tempOp2, "-") == 0) {
+									expr* subExpr = expr_create(EXPR_SUB, leftExpr, rightExpr, 0, '\0', NULL);
+									operatorUsed = 1;
+									printf("the operator used sub\n");
+									//copying right id expr to become new left id expr
+									strcpy(temp, current->value);
+									push_expressionList(expressionListNode, subExpr);
+									current = current->next;
+									if (current == NULL) {
+										printf("current is NULL\n");
+										stmt* ret_stmt = stmt_create(STMT_RETURN, NULL, NULL, NULL, NULL, NULL, NULL, expressionListNode, NULL);
+										push_commandList(commandNode, NULL, ret_stmt, NULL);
+										return current;
+									}
+								}
+								else if (strcmp(tempOp2, "*") == 0) {
+									expr* mulExpr = expr_create(EXPR_MUL, leftExpr, rightExpr, 0, '\0', NULL);
+									operatorUsed = 1;
+									printf("the operator used mul\n");
+									//copying right id expr to become new left id expr
+									strcpy(temp, current->value);
+									push_expressionList(expressionListNode, mulExpr);
+									current = current->next;
+									if (current == NULL) {
+										printf("current is NULL\n");
+										stmt* ret_stmt = stmt_create(STMT_RETURN, NULL, NULL, NULL, NULL, NULL, NULL, expressionListNode, NULL);
+										push_commandList(commandNode, NULL, ret_stmt, NULL);
+										return current;
+									}
+								}
+								else if (strcmp(tempOp2, "/") == 0) {
+									expr* divExpr = expr_create(EXPR_DIV, leftExpr, rightExpr, 0, '\0', NULL);
+									operatorUsed = 1;
+									printf("the operator used div\n");
+									//copying right id expr to become new left id expr
+									strcpy(temp, current->value);
+									push_expressionList(expressionListNode, divExpr);
+									current = current->next;
+									if (current == NULL) {
+										printf("current is NULL\n");
+										stmt* ret_stmt = stmt_create(STMT_RETURN, NULL, NULL, NULL, NULL, NULL, NULL, expressionListNode, NULL);
+										push_commandList(commandNode, NULL, ret_stmt, NULL);
+										return current;
+									}
+								}
+								else {
 
+								}
 							}
-
 						}
-
 					}
-
 					if (strcmp(current->type, "end of command") == 0) {
 						current = current->next;
 						if (current != NULL) {
-							printf("now the current 3 : %s\n", current->value);
+							printf("now the current 3 sub : %s\n", current->value);
 							if (strcmp(current->type, "identifier") == 0) {
 								printf("second parameter after eoc %s\n", current->value);
 								expr* leftExpr = expr_create_string(temp);
